4d8c612b933df75c1c4bb6370a14e67c0d1903f5
karimul <7mdkarimul@gmail.com>
2024-02-15 08:33:58 +0530

    modified firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,15 +19,15 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideToastr(), 
     provideClientHydration(),
-     importProvidersFrom(provideFirebaseApp(() => initializeApp({
-      apiKey: "AIzaSyATJFiJG9y_l8koaAmvURIkJxo1KT3Vqjw",
-      authDomain: "wapcrm-618ce.firebaseapp.com",
-      projectId: "wapcrm-618ce",
-      storageBucket: "wapcrm-618ce.appspot.com",
-      messagingSenderId: "354883501529",
-      appId: "1:354883501529:web:9679008144a225d5412dc8",
-      measurementId: "G-1LWVLKWG7F"
-    }))),
+    //  importProvidersFrom(provideFirebaseApp(() => initializeApp({
+    //   apiKey: "AIzaSyATJFiJG9y_l8koaAmvURIkJxo1KT3Vqjw",
+    //   authDomain: "wapcrm-618ce.firebaseapp.com",
+    //   projectId: "wapcrm-618ce",
+    //   storageBucket: "wapcrm-618ce.appspot.com",
+    //   messagingSenderId: "354883501529",
+    //   appId: "1:354883501529:web:9679008144a225d5412dc8",
+    //   measurementId: "G-1LWVLKWG7F"
+    // }))),
       importProvidersFrom(provideAuth(() => getAuth())),
        importProvidersFrom(provideFirestore(() => getFirestore())), 
        importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"portfolio-411905","appId":"1:143977120072:web:62de3bc05b49569c62f37d","storageBucket":"portfolio-411905.appspot.com","apiKey":"AIzaSyDZrmHFDsptJamnX55IGCbREyeOh-CCyME","authDomain":"portfolio-411905.firebaseapp.com","messagingSenderId":"143977120072"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
--- src/app/external_material/material.module.ts
+++ src/app/external_material/material.module.ts
@@ -1,21 +1,10 @@
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { NgModule } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
-const all = [
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatGridListModule
-    
-];
+
+
 @NgModule({
     declarations: [],
-    imports: [all],
-    exports : [all]
+   
   })
   export class AllInOneModule { }
   
