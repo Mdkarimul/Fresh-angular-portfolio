bb13ba2fc2ead2ae7673d92519cfa5b00e567493
karimul <7mdkarimul@gmail.com>
2026-02-11 19:33:08 +0530

    add projects modification

--- src/app/pages/home/portfolio/portfolio.component.ts
+++ src/app/pages/home/portfolio/portfolio.component.ts
@@ -5,145 +5,81 @@ import { TitleStrategy } from '@angular/router';
 
 @Component({
 
-    selector: 'app-portfolio',
-    templateUrl: './portfolio.component.html',
-    styleUrls: ['./portfolio.component.scss'],
-    standalone: true,
-    imports:[NgFor]
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss'],
+  standalone: true,
+  imports: [NgFor]
 
 })
 export class PortfolioComponent {
 
-  public all:boolean = true;
-  public web:boolean = false;
-  public mobile:boolean =false;
-  public others:boolean = false;
+  public all: boolean = true;
+  public web: boolean = false;
+  public mobile: boolean = false;
+  public others: boolean = false;
 
 
-  project_tab(Ele:string){
-   
-    switch(Ele)
-    {
-      case  'all' :
-      this.all = true;
-      this.mobile = false;
-      this.web =false;
-      this.others = false;
-      break;
+  project_tab(Ele: string) {
 
-      case 'web' :
-        this.web = true;
-        this.all = false;
-        this.mobile =false;
-        this.others =false;
+    switch (Ele) {
+      case 'all':
+        this.all = true;
+        this.mobile = false;
+        this.web = false;
+        this.others = false;
         break;
 
-        case 'mobile' :
-          this.mobile =true;
-          this.all =false;
-          this.others = false;
-          this.web = false;
-          break;
+      case 'web':
+        this.web = true;
+        this.all = false;
+        this.mobile = false;
+        this.others = false;
+        break;
 
-          case 'others' :
-            this.others = true;
-            this.all =false;
-            this.web =false;
-            this.mobile = false;
-            break;
+      case 'mobile':
+        this.mobile = true;
+        this.all = false;
+        this.others = false;
+        this.web = false;
+        break;
 
-            default : 
-            this.all = true
-            break;
+      case 'others':
+        this.others = true;
+        this.all = false;
+        this.web = false;
+        this.mobile = false;
+        break;
+
+      default:
+        this.all = true
+        break;
     }
-}
+  }
 
 
-public projects:any[]  = [
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
+  public projects: any[] = [
 
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
-  {
-    category:"web",
-    img : "../../../assets/Angular_project_one.PNG",
-    title:"responsive angular spa",
-    description:" Html , Css , Bootstrap , Type script , Angular",
-    gitUrl:"",
-    liveUrl:"",
-    feature:true
-  },
 
-] 
+    {
+      category: "web",
+      img: "../../../assets/Angular_project_one.PNG",
+      title: "Angular express crud application",
+      description: "A full stack application built with Angular for frontend and Node.js with Express.js for backend. It performs CRUD operations and is containerized using Docker for easy deployment.",
+      technologies: ["html", "css", "js", "type script", "node js", "express js", "docker", "postgress", "sql"],
+      gitUrl: "https://github.com/Mdkarimul/crud-express-backend.git",
+      liveUrl: "https://github.com/Mdkarimul/crud-express-backend.git",
+      feature: true
+    },
+
+
+
+
+
+
+
+
+  ]
 
 
 
